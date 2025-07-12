a3f04182785c6d34218fee56da10feda0b356ad9
Eddie Cassar <cassar.eddie.l@macbook-pro.lan>
2025-07-12 20:00:24 +0100

    Also applies to top and bottom

--- game_files/GameScreen.py
+++ game_files/GameScreen.py
@@ -15,11 +15,14 @@ class GameScreen():
         # Sets initial position coordinates and velocity and creates player.
         self.player_coords_x = self.width//2
         self.player_coords_y = self.height//2
-        self.player_velocity = 10
-        # Uses the png of a sprite I created using Piskel.
-        self.char_idle = pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_idle.png').convert_alpha()
-        self.char_right = pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_right.png').convert_alpha()
-        self.char_left = pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_left.png').convert_alpha()
+        self.player_velocity = 5
+        # Uses the pngs of sprites I created using Piskel.
+        self.char_idle = pygame.transform.scale(pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_idle.png').convert_alpha(), (64, 64))
+        self.char_right = pygame.transform.scale(pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_right.png').convert_alpha(), (64, 64))
+        self.char_left = pygame.transform.scale(pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_left.png').convert_alpha(), (64, 64))
+        self.char_down = pygame.transform.scale(pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_down.png').convert_alpha(), (64, 64))
+        self.char_up = pygame.transform.scale(pygame.image.load('/Users/cassar.eddie.l/game_project/sprite_sheets/main_character_up.png').convert_alpha(), (64, 64))
+        
         self.game_loop()
 
     def game_loop(self):
@@ -30,7 +33,7 @@ class GameScreen():
         while running:
 
             # Dark red background
-            self.game_screen.fill((128, 0, 0)) 
+            self.game_screen.fill((0, 0, 0)) 
 
             # Sets initial sprite to idle as the player is not moving at the start.
             current_sprite = self.char_idle 
@@ -45,8 +48,10 @@ class GameScreen():
                 current_sprite = self.char_right
             if pressed[pygame.K_UP]:
                self.player_coords_y -= self.player_velocity
+               current_sprite = self.char_up
             if pressed[pygame.K_DOWN]:
                 self.player_coords_y += self.player_velocity
+                current_sprite = self.char_down
 
             # Loads character image
             self.game_screen.blit(current_sprite, (self.player_coords_x, self.player_coords_y))
